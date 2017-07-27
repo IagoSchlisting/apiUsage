d54719ca561cfabea97912ead4cbe74f0b868e9b
iago <iago.machado@kinghost.com.br>
2017-07-27 16:00:58 -0300

    primeiro teste com API

---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+</head>
+	<title>ApiCon TheMovie</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+	<div class="panel">
+	<div class="panel-heading">
+		<table class="table">
+			<form method="get" action="index.php">
+			<tr>
+			<td >Digite um número: </td>
+			<td><input class="form-control" type="number" name="codigo" id="codigo" min="11"></td>
+			<td><button type="submit" class="btn btn-primary">Research</button></td>	
+			</tr>
+			</form>
+		</table>
+	<div class="panel-body">
+	</div>
+	</div>
+</div>
+
+<?php
+
+$url = 'https://api.themoviedb.org/';
+$key = '?api_key=b5147bc48922cf736a3dcc3031ad0e8a';
+$token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhWQiOiJiNTE0N2JjNDg5MjJjZjczNmEzZGNjMzAzMWFkMGU4YSIsInN1YiI6IjU5N2ExNGVlOTI1MTQxMDdjYzAxNzhmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dNtHVxTJ_GRGO9kjiGGUwV2904_Z_CDZf57wyA8-UwQ';
+
+$codigo = $_GET['codigo'];
+$search = '3/movie/'. $codigo;
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url.$search.$key);
+$data = curl_exec($ch);
+curl_close ($ch);
+
+var_dump($data);
+
+?>
+</body>
+</html>
