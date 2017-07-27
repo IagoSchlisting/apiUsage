5323b65e90e4debc9e91da2e994d4e503c05c232
iago <iago.machado@kinghost.com.br>
2017-07-27 17:06:50 -0300

    alguns ajustes

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 	<div class="panel">
 	<div class="panel-heading">
 		<table class="table">
-			<form method="get" action="index.php">
+			<form method="get">
 			<tr>
 			<td >Digite um número: </td>
 			<td><input class="form-control" type="number" name="codigo" id="codigo" min="11"></td>
@@ -22,9 +22,10 @@
 	<div class="panel-body">
 	</div>
 	</div>
-</div>
+	</div>
 
 <?php
+
 
 $url = 'https://api.themoviedb.org/';
 $key = '?api_key=b5147bc48922cf736a3dcc3031ad0e8a';
@@ -33,13 +34,25 @@ $token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhWQiOiJiNTE0N2JjNDg5MjJjZjczNm
 $codigo = $_GET['codigo'];
 $search = '3/movie/'. $codigo;
 
+
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url.$search.$key);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $data = curl_exec($ch);
 curl_close ($ch);
 
-var_dump($data);
+
+$var = json_decode($data);
+
+echo '<pre>';
+echo $var->belongs_to_collection->name . PHP_EOL;
+var_dump($var);
+echo '<pre>';
+
 
 ?>
+
+</div>
 </body>
 </html>
