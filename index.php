670a5870986425303ac3062d64a631f1367936ca
iago <iago.machado@kinghost.com.br>
2017-07-28 14:07:40 -0300

    refatoração do código

--- index.php
+++ index.php
@@ -19,34 +19,8 @@
 			</form>
 		</table>
 	</div>
-<p style="color:green;">Nesse teste você insere um número de 11 á 49998 e ele retorna os dados principais do programa encontrado através da API e um var_dump de todo o resultado para maiores detalhes. &nbsp;&nbsp;&nbsp;&nbsp; <a class="btn btn-info" href="https://www.themoviedb.org/"> TheMovieDB </a></p>
-<br>
-<?php
-
-$url = 'https://api.themoviedb.org/';
-$key = '?api_key=b5147bc48922cf736a3dcc3031ad0e8a';
-$token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhWQiOiJiNTE0N2JjNDg5MjJjZjczNmEzZGNjMzAzMWFkMGU4YSIsInN1YiI6IjU5N2ExNGVlOTI1MTQxMDdjYzAxNzhmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dNtHVxTJ_GRGO9kjiGGUwV2904_Z_CDZf57wyA8-UwQ';
-
-$codigo = $_GET['codigo'];
-$search = '3/movie/'. $codigo;
-
-if($codigo){
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url.$search.$key);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$data = curl_exec($ch);
-curl_close ($ch);
-
-$var = json_decode($data);
-
-$title = $var->original_title;
-$genres = $var->genres;
-$release_data = $var->release_date;
-$production_countries = $var->production_countries;
-$overview = $var->overview;
-
-?>
-
+<p style="color:green;">Nesse teste você insere um número de 11 á 49998 e ele retorna os dados principais do programa encontrado através da API e um var_dump de todo o resultado para maiores detalhes. &nbsp;&nbsp;&nbsp;&nbsp; <a class="btn btn-info" href="https://www.themoviedb.org/"> TheMovieDB </a></p><br>
+<?php if($_GET['codigo']): require_once('apiconn.php'); ?>
 <table class="table table-striped">
 	<tr>
 		<td style="font-weight:600;">Original Title </td>
@@ -66,19 +40,11 @@ $overview = $var->overview;
 	</tr>
 	<tr>
 		<td style="font-weight:bold;">Overview </td>
-		<td><textarea class="form-control" cols="50" rows="5" disabled="true"><?=$overview?>"></textarea></td>
+		<td><textarea class="form-control" cols="50" rows="5" disabled="true"><?=$overview?></textarea></td>
 	</tr>
 </table>
 <br>
-<?php
-
-echo '<pre>';
-
-var_dump($var);
-}
-
-?>
-
+<?php echo '<pre>'; var_dump($var); endif; ?>
 </div>
 </body>
 </html>
