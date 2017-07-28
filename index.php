dfeac2a3d34c50a7adbfd0a2e5dd2ff495f5ab4c
iago <iago.machado@kinghost.com.br>
2017-07-28 10:04:52 -0300

    Ajuste no formulário e Aparência

--- index.php
+++ index.php
@@ -7,23 +7,20 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div class="container">
+<div class="container" style="margin-top:10px;">
 	<div class="panel">
-	<div class="panel-heading">
-		<table class="table">
+		<table class="table table-striped">
 			<form method="get">
 			<tr>
-			<td >Digite um número: </td>
+			<td >Código do Filme: </td>
 			<td><input class="form-control" type="number" name="codigo" id="codigo" min="11"></td>
-			<td><button type="submit" class="btn btn-primary">Research</button></td>	
+			<td><button type="submit" class="btn btn-primary">Search</button></td>	
 			</tr>
 			</form>
 		</table>
-	<div class="panel-body">
 	</div>
-	</div>
-	</div>
-<p style="color:green;">Nesse teste você insere um número de 11 á 49998 e ele retorna os dados principais do programa encontrado através da API &nbsp;&nbsp;&nbsp;&nbsp; <a class="btn btn-info" href="https://www.themoviedb.org/"> TheMovieDB </a></p>
+<p style="color:green;">Nesse teste você insere um número de 11 á 49998 e ele retorna os dados principais do programa encontrado através da API e um var_dump de todo o resultado para maiores detalhes. &nbsp;&nbsp;&nbsp;&nbsp; <a class="btn btn-info" href="https://www.themoviedb.org/"> TheMovieDB </a></p>
+<br>
 <?php
 
 $url = 'https://api.themoviedb.org/';
@@ -32,7 +29,6 @@ $token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhWQiOiJiNTE0N2JjNDg5MjJjZjczNm
 
 $codigo = $_GET['codigo'];
 $search = '3/movie/'. $codigo;
-
 
 if($codigo){
 $ch = curl_init();
@@ -43,7 +39,41 @@ curl_close ($ch);
 
 $var = json_decode($data);
 
+$title = $var->original_title;
+$genres = $var->genres;
+$release_data = $var->release_date;
+$production_countries = $var->production_countries;
+$overview = $var->overview;
+
+?>
+
+<table class="table table-striped">
+	<tr>
+		<td style="font-weight:600;">Original Title </td>
+		<td><input class="form-control" disabled type="text" value="<?=$title?>"></td>	
+	</tr>
+	<tr>
+		<td style="font-weight:bold;">Genres </td>
+		<td><input class="form-control" disabled type="text" value="<?php foreach ($genres as $key => $genre) echo $genre->name . ' - ';?>"></td>			
+	</tr>
+	<tr>
+		<td style="font-weight:bold;">Release Data </td>
+		<td><input class="form-control" disabled type="text" value="<?=$release_data?>"></td>	
+	</tr>
+	<tr>
+		<td style="font-weight:bold;">Production Contries </td>
+		<td><input class="form-control" disabled type="text" value="<?php foreach ($production_countries as $key => $countrie)echo $countrie->name . ' - ';?>"></td>	
+	</tr>
+	<tr>
+		<td style="font-weight:bold;">Overview </td>
+		<td><textarea class="form-control" cols="50" rows="5" disabled="true"><?=$overview?>"></textarea></td>
+	</tr>
+</table>
+<br>
+<?php
+
 echo '<pre>';
+
 var_dump($var);
 }
 
