daf23c6b4ee106d32d1c4795ca95a49851be566a
iago <iago.machado@kinghost.com.br>
2017-08-07 10:50:38 -0300

    Últimas alterações no projeto, inserção de imagens e index principal

---
+++ Controllers/porNota.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once("../Classes/ApiData.php");
+require_once("../Classes/Paginator.php");
+
+function lista($number, $page)
+{
+	session_start();
+	
+	$api = new ApiData();
+	$url = $api->getUrl();
+	$key = $api->getKey();
+
+	$search = '3/movie/top_rated';
+	$page = '&page=' . $number;
+
+	$method = $url.$search.$key.$page;
+	$data = $api->conectaApi($method);
+	$var = $api->transformData($data);
+
+	$_SESSION['data'] = $var->results;
+
+	$paginator = new Paginator($page ,$var->page, $var->total_pages);
+	return $paginator;
+}
+
+?>
